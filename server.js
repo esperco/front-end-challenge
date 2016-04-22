9c7066ba0b26c87079f40bbedb96100682da0b95
Andrew Fong <fongandrew@users.noreply.github.com>
2016-04-22 11:30:20 -0700

    Update methods allowed header

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ var data = [
 // Probably not the safest way to handle CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT');
   res.header("Access-Control-Allow-Headers",
              "Origin, X-Requested-With, Content-Type, Accept");
   next();
